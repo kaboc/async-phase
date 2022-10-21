c30b2d2f855055b8680fe80b8ac7886b6e35d08c
Kabo <20254485+kaboc@users.noreply.github.com>
2022-10-21 19:42:35 +0900

    Remove dispose flag from notifier

--- lib/src/async_phase_notifier.dart
+++ lib/src/async_phase_notifier.dart
@@ -5,14 +5,6 @@ import 'async_phase.dart';
 class AsyncPhaseNotifier<T> extends ValueNotifier<AsyncPhase<T>> {
   AsyncPhaseNotifier(T value) : super(AsyncComplete(data: value));
 
-  bool _active = true;
-
-  @override
-  void dispose() {
-    _active = false;
-    super.dispose();
-  }
-
   @override
   @protected
   set value(AsyncPhase<T> newValue) {
@@ -38,15 +30,11 @@ class AsyncPhaseNotifier<T> extends ValueNotifier<AsyncPhase<T>> {
   Future<AsyncPhase<T>> runAsync(Future<T> Function(T?) func) async {
     value = AsyncWaiting(data: value.data);
 
-    final result = await AsyncPhase.from<T>(
+    value = await AsyncPhase.from<T>(
       () => func(value.data),
       fallbackData: value.data,
     );
 
-    if (_active) {
-      value = result;
-    }
-
-    return result;
+    return value;
   }
 }
